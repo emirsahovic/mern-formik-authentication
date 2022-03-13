4fd6ba733ef7063dd3871a30968ccb5f00a8bc46
emirsahovic <emir.sahovic00@gmail.com>
2022-03-13 13:28:12 +0100

    Error-handling middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import colors from 'colors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import { errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -13,5 +14,7 @@ const PORT = 5000 || process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`.yellow.bold));
