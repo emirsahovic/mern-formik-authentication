141f1f1095067c8748ec9df6d1e9a2873a6f9086
emirsahovic <emir.sahovic00@gmail.com>
2022-03-13 13:53:01 +0100

    App routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
+const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <Router>
+      <Route path='/' element={<Home />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+    </Router>
   );
 }
 
