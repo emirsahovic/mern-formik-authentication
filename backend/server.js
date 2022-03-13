b671a037b8fb8cca1cb572dad1ac0b3ffab65057
emirsahovic <emir.sahovic00@gmail.com>
2022-03-13 13:22:06 +0100

    Database connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import colors from 'colors';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 
 const app = express();
+
+connectDB();
 
 const PORT = 5000 || process.env.PORT;
 
