80ccfb2659aeb37257585a745018f90ab0567b0e
emirsahovic <emir.sahovic00@gmail.com>
2022-03-13 21:12:16 +0100

    Redirect guest to login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
 
 const App = () => {
+  const { user } = useSelector(state => state.auth);
+
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={user ? <Home /> : <Navigate to='/login' />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
       </Routes>
